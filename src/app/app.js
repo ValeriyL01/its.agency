150632c1128853f9f7e93c2ba8354d57438e0a15
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-11-08 10:43:20 +0300

    feat: add slider

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,9 @@
 import createElement from './components/createElement';
+import { swiperСontrol } from './js/slider';
 
 const container = createElement('div', 'container');
 export default function renderApp() {
   document.body.append(container);
   container.append();
+  swiperСontrol();
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.scss';
 
 import './styles/normalize.scss';
+
 import renderApp from './app/app';
 
 renderApp();
